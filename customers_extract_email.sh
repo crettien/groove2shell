0f2ac44b8ba16c76f123da2d6889a540211d8698
Christophe Rettien <crettien@gmail.com>
2019-11-13 18:28:41 +0100

    added customer extract email script

---
+++ customers_extract_email.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author: Christophe Rettien
+# Expected parameters: the json output of customers listing or find
+# Optional parameters: -
+# Usage: ./customers_find_by_name.sh "John DOE" | ./customers_extract_email.sh
+
+function help() {
+  printf "\033[33m# HELP: customers_extract_email\n\033[0m"
+  printf "\033[33m# Expected parameters:\tthe json output of customers listing or find\n\033[0m"
+  printf "\033[33m# Optional parameters:\t-\n\033[0m"
+  printf "\033[33m# Usage:\t\t./customers_find_by_name.sh \"John DOE\" | ./customers_extract_email.sh\n\033[0m"
+}
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+  help
+  exit 0
+fi
+
+while read line; do
+  echo ${line} | grep '"email":' | cut -d':' -f2 | tr -d ', '
+done < /dev/stdin
